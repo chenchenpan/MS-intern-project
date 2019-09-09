e4f69240c085cc77a64bd9c3ff0c6ceb6d7dff55
Chenchen Pan <pcc.pku@gmail.com>
2019-09-09 17:20:02 +0000

    updates for clipped data.

---
+++ run_orig_model_clipdata_tryFasterTuning.sh
@@ -0,0 +1,23 @@
+DATA_FILE=TenantInfo-and-usage_mature_clip_shuffled.csv
+CONFIG_FILE=configure.json
+OUTPUT_DIR_ENCODE=/data/home/t-chepan/projects/MS-intern-project/encoded_data_clip_fast/
+USE_TEXT_FEATURES=FALSE
+OUTPUT_DIR_MODEL=/data/home/t-chepan/projects/MS-intern-project/results_clip_fast/
+TRIAL_NUM=50
+
+# cd ~/projects/MS-intern-project/
+
+# mkdir results_clip_fast
+# mkdir encoded_data_clip_fast
+
+
+# python encoding_data.py --data_file $DATA_FILE \
+#                         --configure_file $CONFIG_FILE \
+#                         --output_dir $OUTPUT_DIR_ENCODE \
+#                         --use_text_features $USE_TEXT_FEATURES
+
+
+
+python NN-hyp-tuning_faster.py --data_dir $OUTPUT_DIR_ENCODE \
+                        --output_dir $OUTPUT_DIR_MODEL \
+                        --trial_num $TRIAL_NUM
